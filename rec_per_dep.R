a935da65bd4563d352ad6806832d29896e5d2809
lromang <luis.roangarci@gmail.com>
2015-12-01 18:52:23 -0600

    url

--- rec_per_dep.R
+++ rec_per_dep.R
@@ -70,6 +70,9 @@ id_conj <- str_replace_all(id_conj,"é","e")
 id_conj <- str_replace_all(id_conj,"í","i")
 id_conj <- str_replace_all(id_conj,"ó","o")
 id_conj <- str_replace_all(id_conj,"ú","u")
+id_conj <- str_replace_all(id_conj,"ñ","n")
+id_conj <- str_replace_all(id_conj,"datosgobmx","datos-gob-mx")
+id_conj <- str_replace_all(id_conj,"sa-de-cv","s-a-de-cv")
 con_url     <- dataset_url <- paste0("http://busca.datos.gob.mx/#/conjuntos/",id_conj)
 for(i in 1:nrow(facts)){
     if(is.na(facts$url[i])){
